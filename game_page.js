f596006cb30112557ede037b4d8789201a07625c
Dhriti2010 <89203702+Dhriti2010@users.noreply.github.com>
2022-03-25 16:54:34 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -20,3 +20,35 @@ function send(){
     document.getElementById("number1").value="";
     document.getElementById("number2").value="";
 }
+questionturn="player1";
+answerturn="player2";
+function check(){
+    getanswer=document.getElementById("input_check_box").value;
+    if (getanswer==actualanswer){
+        if (answerturn=="player1"){
+            player1score=player1score+1;
+            document.getElementById("player1_score").innerHTML=player1score;
+        }
+        else {
+            player2score=player2score+1;
+            document.getElementById("player2_score").innerHTML=player2score;
+        }
+    }
+    if (questionturn=="player1"){
+        questionturn="player2";
+        document.getElementById("player_question").innerHTML="Question turn- "+player2name;
+    }
+    else {
+        questionturn="player1";
+        document.getElementById("player_question").innerHTML="Question turn- "+player1name;
+    }
+    if (answerturn=="player1"){
+        answerturn="player2";
+        document.getElementById("player_answer").innerHTML="Answer turn- "+player2name;
+    }
+    else {
+        answerturn="player1";
+        document.getElementById("player_answer").innerHTML="Answer turn- "+player1name;
+    }
+    document.getElementById("output").innerHTML="";
+}
